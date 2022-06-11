b4d3f541c0bc8cd9533afe7b1e232fb9fa5c6818
Moses Karani <karanim594@gmail.com>
2022-06-11 20:40:39 +0300

    created search and project view

--- miawwards/views.py
+++ miawwards/views.py
@@ -1,9 +1,12 @@
+from email import message
+from math import fabs
 import random
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate
-from miawwards.forms import PostForm, SignUpForm, UpdateUserForm, UpdateUserProfileForm
-from miawwards.models import Post
+from miawwards.forms import PostForm, RatingsForm, SignUpForm, UpdateUserForm, UpdateUserProfileForm
+from miawwards.models import Post, Rating
 
 
 
@@ -59,3 +62,57 @@ def edit_profile(request, username):
             'prof_form': prof_form
         }
         return render(request, 'edit.html', context)
+
+
+def project(request, post):
+    post = Post.objects.get(title=post)
+    ratings = Rating.objects.filter(user=request.user, post=post).first()
+    rating_status = None
+    if ratings is None:
+        rating_status = False
+    else:
+        rating_status = True
+    if request.method == 'POST':
+        form = RatingsForm(request.POST)
+        if form.is_valid():
+            rate = form.save(commit=False)
+            rate.user = request.user
+            rate.post = post
+            rate.save()
+            post_ratings = Rating.objects.filter(post=post)
+
+            design_ratings = [d.design for d in post_ratings]
+            design_average = sum(design_ratings) / len(design_ratings)
+
+            usability_ratings = [us.usability for us in post_ratings]
+            usability_average = sum(usability_ratings) / len(usability_ratings)
+
+            content_ratings = [content.content for content in post_ratings]
+            content_average = sum(content_ratings) / len(content_ratings)
+
+            score = (design_average + usability_average + content_average) / 3
+            rate.design_average = round(design_average, 2)
+            rate.usability_average = round(usability_average, 2)
+            rate.content_average  = round(content_average, 2)
+            rate.score = round(score, 2)
+            rate.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        form = RatingsForm()
+    context = {
+        'post':post,
+        'rating_form':form,
+        'rating_status':rating_status
+    }
+    return render(request, 'project.html', context)
+
+def search_project(request):
+    if request.method == 'GET':
+        title = request.GET.get('title')
+        results = Post.objects.filter(title__icontains=title).all()
+        message = f'name'
+        context = {'results':results, 'message':message}
+        return render(request, 'results.html', context)
+    else:
+        message = "You haven't searched for anything!"
+    return render(request, 'results.html', {'message':message})
