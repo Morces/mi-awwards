38df791789cb8134601144aa9eec08a8aa15ba86
Moses Karani <karanim594@gmail.com>
2022-06-11 22:41:14 +0300

    optimized authentication templates

--- miawwards/urls.py
+++ miawwards/urls.py
@@ -1,10 +1,12 @@
 from . import views
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('profile/<username>/', views.profile, name='profile'),
     path('<username>/profile', views.user_profile, name='userprofile'),
     path('profile/<username>/settings', views.edit_profile, name='edit'),
