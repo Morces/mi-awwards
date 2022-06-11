63710afede64de48daf19cb4b6b2f01d99ccbf1b
Moses Karani <karanim594@gmail.com>
2022-06-11 22:06:40 +0300

    defined pathe for search and project

--- miawwards/urls.py
+++ miawwards/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('profile/<username>/', views.profile, name='profile'),
     path('<username>/profile', views.user_profile, name='userprofile'),
     path('profile/<username>/settings', views.edit_profile, name='edit'),
+    path('project/<post>', views.project, name='project'),
+    path('search/', views.search_project, name='search')
 ]
