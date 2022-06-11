55391c115e82a831133cb82376fb44452a8532ec
Moses Karani <karanim594@gmail.com>
2022-06-11 17:36:03 +0300

    created a useredit form and a template

--- miawwards/urls.py
+++ miawwards/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('profile/<username>/', views.profile, name='profile'),
+    path('<username>/profile', views.user_profile, name='userprofile')
 ]
--- miawwards/views.py
+++ miawwards/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.models import User
 
+from miawwards.forms import UpdateUserForm, UpdateUserProfileForm
+
 
 
 # Create your views here.
@@ -21,4 +23,22 @@ def user_profile(request, username):
 
     }
     return render(request, 'userprofile.html', context)
-    
+
+
+def edit_profile(request, username):
+    user = User.objects.get(username=username)
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        prof_form = UpdateUserProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and prof_form.is_valid():
+            user_form.save()
+            prof_form.save()
+            return redirect('profile', user.username)
+        else:
+            user_form = UpdateUserForm(instance=request.user)
+            prof_form = UpdateUserProfileForm(instance=request.user.profile)
+        context = {
+            'user_form': user_form,
+            'prof_form': prof_form
+        }
+        return render(request, 'edit.html', context)
