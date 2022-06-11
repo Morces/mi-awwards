5b229980f5f8c113281a590abfafd8f034a1bea2
Moses Karani <karanim594@gmail.com>
2022-06-11 17:37:39 +0300

    added a url for edit profile

--- miawwards/urls.py
+++ miawwards/urls.py
@@ -5,5 +5,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.index, name='index'),
     path('profile/<username>/', views.profile, name='profile'),
-    path('<username>/profile', views.user_profile, name='userprofile')
+    path('<username>/profile', views.user_profile, name='userprofile'),
+    path('profile/<username>/settings', views.edit_profile, name='edit'),
 ]
