26ed70800569d7a0d0a791591f086f04b238aa40
Moses Karani <karanim594@gmail.com>
2022-06-11 17:04:01 +0300

    created a view for profile and templates

--- miawwards/views.py
+++ miawwards/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.models import User
 
 
 
@@ -9,5 +10,15 @@ def index(request):
 
 
 def profile(request, username):
-    return render(request, 'profile.html')
+    return render(request, 'profile.html', username)
 
+def user_profile(request, username):
+    user_prof = get_object_or_404(User, username=username)
+    if request == user_prof:
+        return redirect('profile', username=username)
+    context = {
+        'user_prof':user_prof
+
+    }
+    return render(request, 'userprofile.html', context)
+    
